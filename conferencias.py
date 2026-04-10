e21e6f5f3cd3a25c3d1d7d381c926691651e0b45
Jader Santiago Nieves <jsnieves39@ucatolica.edu.co>
2026-04-09 21:42:45 -0500

    Ejercicio Conferencia

--- conferencias.py
+++ conferencias.py
@@ -1,16 +1,16 @@
 def seleccion_actividades(actividades):
-    # ORDENAR LAS ACTIVIDADES POR SU HORA DE FIN
-    actividades.sort(key=lambda x: x[1])
+    # ordenar por tiempo de finalización
+    actividades.sort(key=lambda x: x[2])
     
     seleccionadas = []
     
-    # SELECCIONAR LA PRIMERA ACTIVIDAD
+    # tomar la primera
     seleccionadas.append(actividades[0])
-    ultima_fin = actividades[0][1]
+    ultima_fin = actividades[0][2]
     
-    # RECORRER LAS ACTIVIDADES RESTANTES
+    # recorrer las demás
     for i in range(1, len(actividades)):
-        inicio, fin = actividades[i]
+        nombre, inicio, fin = actividades[i]
         
         if inicio >= ultima_fin:
             seleccionadas.append(actividades[i])
@@ -19,21 +19,19 @@ def seleccion_actividades(actividades):
     return seleccionadas
 
 
+# Datos (con nombres)
 actividades = [
-    (8.0, 9.0),   # CONFERENCIA 1
-    (8.5, 10.0),  # CONFERENCIA 2
-    (9.0, 10.0),  # CONFERENCIA 3
-    (10.0, 11.0), # CONFERENCIA 4
-    (9.5, 11.5),  # CONFERENCIA 5
-    (11.0, 12.0)  # CONFERENCIA 6
+    ("Conferencia 1", 8.0, 9.0),
+    ("Conferencia 2", 8.5, 10.0),
+    ("Conferencia 3", 9.0, 10.0),
+    ("Conferencia 4", 10.0, 11.0),
+    ("Conferencia 5", 9.5, 11.5),
+    ("Conferencia 6", 11.0, 12.0)
 ]
 
 resultado = seleccion_actividades(actividades)
 
-print("Actividades seleccionadas:")
-for act in resultado:
-    print(act)
-    
-    
-    
-    
+print("Actividades seleccionadas:\n")
+
+for nombre, inicio, fin in resultado:
+    print(f"{nombre} → Inicio: {inicio} | Fin: {fin}")
